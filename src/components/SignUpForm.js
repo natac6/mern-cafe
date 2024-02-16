2e90520ffd9c319017d55910c537620346078904
Teo Marcelo <tmarcelojr@gmail.com>
2024-02-15 19:28:49 -0600

    added utilities folder

---
+++ src/components/SignUpForm.js
@@ -0,0 +1,176 @@
+// import { Component } from "react";
+
+// export default class SignUpForm extends Component {
+//   // Classes are not used anymore in React. This is for you to learn legacy code
+//   // Below is old school way of declaring your state object. This is similar to how other OOP programming languages work.
+//   // constructor() {
+//   //   this.state = {
+//   //     name: "",
+//   //     email: "",
+//   //     password: "",
+//   //     confirm: "",
+//   //     error: "",
+//   //   };
+//   // }
+
+//   // More modern way of writing state object
+//   state = {
+//     name: "",
+//     email: "",
+//     password: "",
+//     confirm: "",
+//     error: "",
+//   };
+
+//   // The object passed to setState is merged with the current state object
+//   handleChange = (event) => {
+//     // e or evt or event
+//     // console.log(event.target)
+//     this.setState({
+//       // email: 'teo@teo.com
+//       // password: "****"
+//       [event.target.name]: event.target.value,
+//     });
+//   };
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//   };
+
+//   render() {
+//     const disable = this.state.password !== this.state.confirm;
+//     return (
+//       <div>
+//         <div className="form-container">
+//           <form autoComplete="off" onSubmit={this.handleSubmit}>
+//             <label>Name</label>
+//             <input
+//               type="text"
+//               name="name"
+//               value={this.state.name}
+//               onChange={this.handleChange}
+//             //   required
+//             />
+//             <label>Email</label>
+//             <input
+//               type="email"
+//               name="email"
+//               value={this.state.email}
+//               onChange={this.handleChange}
+//             //   required
+//             />
+//             <label>Password</label>
+//             <input
+//               type="password"
+//               name="password"
+//               value={this.state.password}
+//               onChange={this.handleChange}
+//             //   required
+//             />
+//             <label>Confirm</label>
+//             <input
+//               type="password"
+//               name="confirm"
+//               value={this.state.confirm}
+//               onChange={this.handleChange}
+//             //   required
+//             />
+//             <button type="submit" >
+//               SIGN UP
+//             </button>
+//           </form>
+//         </div>
+//         <p className="error-message">&nbsp;{this.state.error}</p>
+//       </div>
+//     );
+//   }
+// }
+
+
+// Refactor to Functional Component
+import { useState } from 'react';
+
+const SignUpForm = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirm: ""
+    })
+
+    const [error, setError] = useState("")
+
+    const handleChange = e => {
+        const { name, value } = e.target
+
+        setFormData({...formData, [name]: value})
+
+        // setFormData(prevFormData => {
+        //     return (
+        //         {
+        //             ...prevFormData,
+        //             [name] : value
+        //         }
+        //     )
+        // })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        try {
+            console.log(formData)
+
+        } catch(error) {
+            console.log(error)
+        }
+    }
+
+    const disable = formData.password !== formData.confirm;
+
+    return (
+        <div>
+         <div className="form-container">
+           <form autoComplete="off" onSubmit={handleSubmit}>
+             <label>Name</label>
+             <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+            <label>Email</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <label>Confirm</label>
+            <input
+              type="password"
+              name="confirm"
+              value={formData.confirm}
+              onChange={handleChange}
+              required
+            />
+            <button type="submit" >
+              SIGN UP
+            </button>
+          </form>
+        </div>
+        <p className="error-message">&nbsp;{error}</p>
+      </div>
+    )
+}
+
+export default SignUpForm
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,5 +1,11 @@
+import SignUpForm from "../components/SignUpForm"
+
 export default function Auth () {
   return (
-    <h1>Auth Page</h1>
+    <>
+      <h1>Auth Page</h1>
+      <SignUpForm />
+    </>
+    
   )
 }
